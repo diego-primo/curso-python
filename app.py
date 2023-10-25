43d33c8693f7d63d145e2d8c6e8c570dff4bf275
Diego Primo Guimarães Dias <dpdias@stefanini.com.br>
2023-10-25 00:08:39 -0300

    até a aula 11 da playlist

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 print('Hello Word!')
 
-nome = "Diego"
-print(nome)
+nome = input('Qual é o seu nome: ')
+idade = input('Qual é sua idade: ')
+peso = input('Qual é seu peso: ')
+
+print(nome, idade, peso)
